7e8b58c829073298fb363d6b99f45501783c5ac0
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-02-13 11:54:49 +0900

    Initial Commit

---
+++ SwiftUIRouterSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIRouterSample
+//
+//  Created by Yusuke Hasegawa on 2021/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIRouterSample/SwiftUIRouterSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRouterSampleApp.swift
+//  SwiftUIRouterSample
+//
+//  Created by Yusuke Hasegawa on 2021/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRouterSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
